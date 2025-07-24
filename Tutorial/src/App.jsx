96d1740ee68ed2ecdf9f9bfaed205b3b0a4855da
Manav <manavboghraa12@gmail.com>
2025-07-24 17:31:07 +0530

    Modify the App.jsx

--- Tutorial/src/App.jsx
+++ Tutorial/src/App.jsx
@@ -11,33 +11,65 @@ import { App1 } from "./components/Toggle";
 import { Greeting } from "./components/Greeting";
 import Chatting from "./components/Chatting";
 // import { ChatRoom } from "./components/Chatting";
+import Welcome from "./components/Welcome";
+import { DataExtract } from "./components/DataExtract";
+import Form from "./components/Form";
+import { Conditional } from "./components/Conditional";
+import { Render } from "./components/Render";
+import { Referencing } from "./components/Referencing";
+import { Counter } from "./components/Counter";
+import { Hover } from "./components/Hover";
+import { Wrapper } from "./components/Wrapper";
 
 // import createconnection from "./components/Chat";
 
-
-
 function App() {
-
   return (
     <>
       <div>
-        {/* <Header />
+        {/* <Header /> */}
         <User />
-        <MyButton/>
-        <UserImage/> */}
-        {/* <List/>
-        <App1/>
-        <Greeting user="Admin"/> */}
-        <Chatting/> 
+        {/* <MyButton/> */}
+        {/* <UserImage/> */}
+        {/* <List/> */}
+        {/* <App1/> */}
+        {/* <Greeting user="Admin"/> */}
+        {/* <Chatting/>  */}
         {/* <ChatRoom/> */}
-        
-        
-        
+        {/* <Welcome/> */}
+        {/* <DataExtract/> */}
+        {/* <Form/> */}
+        {/* <Conditional/> */}
+        {/* <Rander name = {(isLoggedin)=> isLoggedin ?"Admin":"Guest" }/> */}
+        {/* <Referencing/> */}
+        {/* <Counter/>
+        <Hover/> */}
+        <Wrapper
+          render={({ count, handleClick }) => (
+            <>
+              <Counter count={count} handleClick={handleClick} />
+            </>
+          )}
+        />
+        <Wrapper
+          render={({ count, handleClick }) => (
+            <>
+              <Hover count={count} handleClick={handleClick} />
+            </>
+          )}
+        />
+
+        {/* <Wrapper rander = {(count, handleClick)=>{<Counter count={count} handleClick={handleClick}/>}}/> */}
+
+        <Render
+          onFlag={false}
+          Rander={(isOn) => {
+            return isOn ? <div>Toggle is On </div> : <div>Toggle is off </div>;
+          }}
+        />
       </div>
     </>
   );
 }
-
-
 
 export default App;
