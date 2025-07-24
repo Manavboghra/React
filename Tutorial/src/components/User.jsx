3f4a70483a29c898a9517430c3e260491b8afa41
Manav <manavboghraa12@gmail.com>
2025-07-24 12:12:30 +0530

    fix the error

--- Tutorial/src/components/User.jsx
+++ Tutorial/src/components/User.jsx
@@ -4,7 +4,7 @@ export const User = () => {
   const handleNameChange = () => {
     const UserName = ["Joshph", "Tokiee", "Bambi"];
     const randomName = Math.floor(Math.random() * 3);
-    return randomName[UserName]
+    return UserName[randomName]
   };  
   return <div>Hello {handleNameChange()}</div>;
 };
