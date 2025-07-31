19c65fa155dc625e3ab143a368bccf16ccabd17c
Manav <manavboghraa12@gmail.com>
2025-07-31 17:42:55 +0530

    Modified the code

--- Tutorial/src/components/Counter.jsx
+++ Tutorial/src/components/Counter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 export const Counter = ({count,handleClick}) => {
+  console.log("I am changing")
   return (
     <>
       <button onClick={handleClick}>Click {count} times</button>
