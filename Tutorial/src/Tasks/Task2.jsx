3b2fd2da0f17f929fe53edbb01bc82eb0486d5b1
Manav <manavboghraa12@gmail.com>
2025-08-04 17:41:06 +0530

    add useContext to extract password

--- Tutorial/src/Tasks/Task2.jsx
+++ Tutorial/src/Tasks/Task2.jsx
@@ -1,13 +1,18 @@
-import React, { useState, useCallback, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  createContext,
+} from "react";
+import { ComponentA } from "../components/Hooks/Context/ComponentA";
+export const PasswordContext = createContext("Admin");
 
 export const Passgenerator = () => {
   const [count, setCount] = useState(0);
   const [number, setNumber] = useState(false);
   const [char, setChar] = useState(false);
-  const [pass, setPass] = useState(
-   localStorage.getItem("password") || ""
-  );
-
+  const [pass, setPass] = useState(localStorage.getItem("password") || "");
   useEffect(() => {
     localStorage.setItem("password", pass);
   }, [pass]);
@@ -34,69 +39,73 @@ export const Passgenerator = () => {
     }
     setPass(generatedPassword);
   }, [count, number, char]);
-
   useEffect(() => {
     generatePassword();
   }, [count, number, char, generatePassword]);
 
   return (
-    <div className="w-full max-w-md mx-auto shadow-lg rounded-lg px-4 py-5 my-8 bg-gray-800 text-orange-500">
-      <h1 className="text-white text-center text-2xl mb-4">
-        Password Generator
-      </h1>
+    <PasswordContext.Provider value={pass}>
+      <div className="w-full max-w-md mx-auto shadow-lg rounded-lg px-4 py-5 my-8 bg-gray-800 text-orange-500">
+        <h1 className="text-white text-center text-2xl mb-4">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={pass}
+            className="outline-none w-full py-2 px-3 bg-gray-700 text-white"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={handleCopy}
+            className="outline-none bg-blue-700 text-white px-4 py-1 shrink-0 hover:bg-blue-600 active:bg-blue-800"
+          >
+            Copy
+          </button>
+        </div>
 
-      <div className="flex shadow rounded-lg overflow-hidden mb-4">
-        <input
-          type="text"
-          value={pass}
-          className="outline-none w-full py-2 px-3 bg-gray-700 text-white"
-          placeholder="Password"
-          readOnly
-          ref={passwordRef}
-        />
-        <button
-          onClick={handleCopy}
-          className="outline-none bg-blue-700 text-white px-4 py-1 shrink-0 hover:bg-blue-600 active:bg-blue-800"
-        >
-          Copy
-        </button>
+        <div className="flex flex-wrap text-sm gap-x-4 gap-y-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={12}
+              value={count}
+              className="cursor-pointer"
+              onChange={(e) => setCount(e.target.value)}
+            />
+            <label className="text-white">Length: {count}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              checked={number}
+              id="numberInput"
+              onChange={() => setNumber((prev) => !prev)}
+            />
+            <label htmlFor="numberInput" className="text-white cursor-pointer">
+              Numbers
+            </label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              checked={char}
+              id="characterInput"
+              onChange={() => setChar((prev) => !prev)}
+            />
+            <label
+              htmlFor="characterInput"
+              className="text-white cursor-pointer"
+            >
+              Characters
+            </label>
+          </div>
+        </div>
+        <ComponentA />
       </div>
-
-      <div className="flex flex-wrap text-sm gap-x-4 gap-y-2">
-        <div className="flex items-center gap-x-1">
-          <input
-            type="range"
-            min={6}
-            max={12}
-            value={count}
-            className="cursor-pointer"
-            onChange={(e) => setCount(e.target.value)}
-          />
-          <label className="text-white">Length: {count}</label>
-        </div>
-        <div className="flex items-center gap-x-1">
-          <input
-            type="checkbox"
-            checked={number}
-            id="numberInput"
-            onChange={() => setNumber((prev) => !prev)}
-          />
-          <label htmlFor="numberInput" className="text-white cursor-pointer">
-            Numbers
-          </label>
-        </div>
-        <div className="flex items-center gap-x-1">
-          <input
-            type="checkbox"
-            checked={char}
-            id="characterInput"
-            onChange={() => setChar((prev) => !prev)}
-          />
-          <label htmlFor="characterInput" className="text-white cursor-pointer">
-            Characters
-          </label>
-        </div>
-      </div>
-    </div>
+    </PasswordContext.Provider>
   );
 };
