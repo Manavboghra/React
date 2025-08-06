f100d9f654c9b95768a353a55201446a4cad18a0
Manav <manavboghraa12@gmail.com>
2025-08-06 21:41:05 +0530

    Header file added

---
+++ Currency-Converter/Currency-Converter/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 shadow-lg sticky top-0 z-10">
+      <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
+        <div className="text-2xl font-bold text-blue-400">
+          CurrencyApp
+        </div>
+        <div className="flex items-center space-x-6">
+          <NavLink to={"/Home"} className={({isActive})=> `${isActive ? "text-orange-500" : "text-gray-300"}   hover:text-blue-400 transition-colors`} >Home</NavLink>
+          <NavLink to={"/CurrencyConverter"} className={({isActive})=> `${isActive ? "text-orange-500" : "text-gray-300"}   hover:text-blue-400 transition-colors`} >CurrencyConverter</NavLink>
+          <NavLink to={"/Denominator"}  className={({isActive})=> `${isActive ? "text-orange-500" : "text-gray-300"}   hover:text-blue-400 transition-colors`} >Denominator</NavLink>
+          <NavLink to={"/Reverse"}  className={({isActive})=> `${isActive ? "text-orange-500" : "text-gray-300"}   hover:text-blue-400 transition-colors`} >Reverse</NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
