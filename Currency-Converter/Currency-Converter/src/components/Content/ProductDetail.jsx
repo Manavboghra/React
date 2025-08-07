a575ece4567edcc835edeffb495a5208d687d0c7
Manav <manavboghraa12@gmail.com>
2025-08-07 16:59:21 +0530

    Add products components

---
+++ Currency-Converter/Currency-Converter/src/components/Content/ProductDetail.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useParams, Link, useLoaderData } from "react-router";
+
+export const ProductDetail = () => {
+  //   const [product, setProduct] = useState(null);
+
+  const product = useLoaderData();
+  // const { productId } = useParams();
+
+  //   useEffect(() => {
+  //     fetch(`https://dummyjson.com/products/${productId}`)
+  //       .then((res) => res.json())
+  //       .then((data) => setProduct(data));
+  //   }, [productId]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="p-10">
+      <Link to="/products" className="text-blue-500 hover:underline mb-4 block">
+         Back to Products
+      </Link>
+      <div className="flex flex-col md:flex-row gap-8">
+        <img
+          src={product.images[0]}
+          alt={product.title}
+          className="w-full md:w-1/2 rounded-lg shadow-lg"
+        />
+        <div>
+          <h1 className="text-4xl font-bold">{product.title}</h1>
+          <p className="text-xl text-gray-700 my-4">{product.description}</p>
+          <p className="text-3xl font-light">${product.price}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const ProductDetailLoader = async ({ params }) => {
+  const productId = params.productId;
+  const response = await fetch(`https://dummyjson.com/products/${productId}`);
+  const data = await response.json();
+  return data;
+};
